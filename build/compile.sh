70da7a9e8d11147533c608c486b57387ddf25070
Soaringxmc <maochao_xiao@outlook.com>
2024-01-30 20:42:05 +0100

    successful compilation using nvfortran

--- build/compile.sh
+++ build/compile.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# cfl3d_mpi for parallel version, cfl3d_seq for sequential version
-# makefile_gfortran for GNU compiler, and makefile_ifort for INTEL compiler
 # use -j on cluster AEROLAB, and use -j 8 on cluster TIANHE
-# define -DFASTIO in makefile for cfl3d_mpi, 
-# and do not define if for cfl3d_seq
-make scruball -f makefile_gfortran
-make linkall -f makefile_gfortran
-make cfl3d_libs -f makefile_gfortran -j
-make cfl3d_mpi  -f makefile_gfortran -j
+# define -DFASTIO for cfl3d_mpi, and not for cfl3d_seq in makefile
+make scruball -f makefile_nvidia
+make linkall -f makefile_nvidia
+make cfl3d_libs -f makefile_nvidia -j
+make cfl3d_mpi  -f makefile_nvidia -j
