fd02318e7958461a4d858cb45c4ff8c386d546db
Soaringxmc <maochao_xiao@outlook.com>
2024-01-31 11:29:49 +0100

    compile.sh modified

--- build/compile.sh
+++ build/compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # -j on cluster AEROLAB, and -j 8 recommended on cluster TIANHE
 # In makefile, add -DFASTIO if cfl3d_mpi, but remove it if cfl3d_seq
-make scruball -f makefile_nvidia
-make linkall -f makefile_nvidia
-make cfl3d_libs -f makefile_nvidia -j
-make cfl3d_mpi  -f makefile_nvidia -j
+
+make scruball -f makefile_gnu
+make linkall -f makefile_gnu
+make cfl3d_libs -f makefile_gnu -j
+make cfl3d_mpi  -f makefile_gnu -j
