f3cd5ae6a1890286fdb4d18757d68f1e748fafb1
Soaringxmc <maochao_xiao@outlook.com>
2024-06-13 09:26:49 +0200

    intel, use mpiifx, mpiicx

--- build/compile.sh
+++ build/compile.sh
@@ -2,7 +2,7 @@
 # -j on cluster AEROLAB, and -j 8 recommended on cluster TIANHE
 # In makefile, add -DFASTIO if cfl3d_mpi, but remove it if cfl3d_seq
 
-make scruball -f makefile_gnu
-make linkall -f makefile_gnu
-make cfl3d_libs -f makefile_gnu -j
-make cfl3d_mpi  -f makefile_gnu -j
+make scruball -f makefile_intel -j
+make linkall -f makefile_intel -j
+make cfl3d_libs -f makefile_intel -j
+make cfl3d_mpi  -f makefile_intel -j
