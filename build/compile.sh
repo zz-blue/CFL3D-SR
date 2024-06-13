e290963351438da19457f2dde83e734ec3cd34cb
Soaringxmc <maochao_xiao@outlook.com>
2024-06-13 11:10:20 +0200

    && added to compile.sh

--- build/compile.sh
+++ build/compile.sh
@@ -2,7 +2,7 @@
 # -j on cluster AEROLAB, and -j 8 recommended on cluster TIANHE
 # In makefile, add -DFASTIO if cfl3d_mpi, but remove it if cfl3d_seq
 
-make scruball -f makefile_intel -j
-make linkall -f makefile_intel -j
-make cfl3d_libs -f makefile_intel -j
-make cfl3d_mpi  -f makefile_intel -j
+make scruball -f makefile_intel -j &&
+make linkall -f makefile_intel -j &&
+make cfl3d_libs -f makefile_intel &&
+make cfl3d_mpi  -f makefile_intel -j &&
